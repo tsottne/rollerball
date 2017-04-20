dbce60d0329ffc720ab0bf871fd5d2c6e0dd754e
206-25\Student <doriuheki@gmail.com>
2017-04-20 20:56:16 +0400

    no message

---
+++ Assets/test2.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+public class test2 : MonoBehaviour
+{
+    public int a = 5;
+    public float b = 0.645632f;
+    public bool c = true;
+    public Vector3 scspeed;
+    public Vector3 speed;
+
+
+
+    void Start()
+    {
+
+        
+    }
+
+    void Update() {
+        transform.localScale = transform.localScale + speed;
+        transform.position = transform.position + scspeed;
+
+    }
+
+}
+   
+
